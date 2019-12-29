ede0c42c8eccd1a5eba10d47880a7ce7c4db3217
fukun <heshi_fk@163.com>
2019-12-29 17:14:14 +0800

    重现启用hystrix，feign调用的问题

--- feign-api1/src/main/java/com/heshi/api1/client/Ref01Client.java
+++ feign-api1/src/main/java/com/heshi/api1/client/Ref01Client.java
@@ -1,12 +1,13 @@
 package com.heshi.api1.client;
 
+import com.heshi.api1.config.FeignConfig;
 import com.heshi.api1.fallback.Ref01Fallback;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @ConditionalOnMissingClass("com.heshi.service01.provider.Ref01Provider")
-@FeignClient(name = "ref011111111", fallback = Ref01Fallback.class)
+@FeignClient(name = "ref01", fallback = Ref01Fallback.class, configuration = FeignConfig.class)
 public interface Ref01Client {
     String prefix = "/v1";
 
---
+++ feign-api1/src/main/java/com/heshi/api1/config/FeignConfig.java
@@ -0,0 +1,25 @@
+package com.heshi.api1.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Configuration
+public class FeignConfig implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            template.header(name, value);
+        }
+    }
+}
